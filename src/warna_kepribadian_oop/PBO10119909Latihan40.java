67f6191cf35092dcab87a51cd4af929cf468fd2e
anggawanridho <anggawanridho@gmail.com>
2020-11-01 15:40:44 +0700

    first commit

---
+++ src/warna_kepribadian_oop/PBO10119909Latihan40.java
@@ -0,0 +1,18 @@
+package warna_kepribadian_oop;
+
+/**
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+
+public class PBO10119909Latihan40 {
+
+    public static void main(String[] args) {
+        Kepribadian orang = new Kepribadian();
+        orang.getNama();
+        orang.getWarna();
+        orang.getKepribadian();
+    }
+}
